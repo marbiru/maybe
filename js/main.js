3d193ce63c33ce523039e0d5b2683f5ae8488c4d
marbiru <code@uribram.com>
2015-07-29 14:13:59 +0100

    feat: more text effects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(function () {
-	$('.tlt').textillate({ in: { effect: 'rollIn' } });
-})
+	$('.tlt_headlines').textillate({ 
+		in: { 
+			effect: 'rollIn',
+			delayScale: 1, 
+		} 
+	});
+});
+
+$(function () {
+	$('.tlt_source').textillate({ 
+		initialDelay: 1500,
+		in: {
+		 effect: 'rollIn',
+		 delayScale: 0.3,
+		 shuffle: true,
+		}
+	});
+});
