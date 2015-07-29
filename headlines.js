7ca91b64d7dd76d8c66fc8f27f977e27710beaf9
marbiru <code@uribram.com>
2015-07-29 15:55:21 +0100

    feat: favicon, tweaks, new objects

---
+++ headlines.js
@@ -0,0 +1,59 @@
+var insulin = {
+	headline: "Insulin resistance might increase Alzheimer’s risk", 
+	url: "http://www.reuters.com/article/2015/07/28/us-health-alzheimers-diabetes-idUSKCN0Q22I720150728", 
+	source:"Reuters, 28 July 2015",
+};
+
+var streetlights = {
+	headline: "Streetlights Might Not Make You Safer",
+	url: "http://www.psmag.com/politics-and-law/streetlights-might-not-make-you-safer",
+	source: "Pacific Standard, 28 July 2015",
+};
+
+var corbyn = {
+	headline: "Why China’s Stock Market Bailout Just Might Work",
+	url: "http://www.nytimes.com/2015/07/10/business/international/why-chinas-stock-market-bailout-just-might-work.html",
+	source: "New York Times, 9 July 2015",
+};
+
+var maybe = [insulin, streetlights, corbyn];
+
+var coach = {
+	headline: "Mexico's Soccer Coach Loses Job After Allegedly Punching Reporter",
+	url: "http://www.npr.org/sections/thetwo-way/2015/07/28/427208951/mexico-soccer-coach-loses-job-after-allegedly-punching-reporter",
+	source: "NPR, 28 July 2015",
+};
+
+var lord = {
+	headline: "British Lord Resigns After Video Emerges of Him Allegedly Snorting Cocaine, Partying with Prostitutes",
+	url: "http://www.vanityfair.com/news/2015/07/british-lord-resigns-after-cocaine-prostitutes-video",
+	source: "Vanity Fair, 28 July 2015",
+};
+
+var conan = {
+	headline: "'Conan' Is Being Sued For Allegedly Stealing Jokes From Twitter",
+	url: "http://www.huffingtonpost.com/entry/conan-lawsuit-stolen-jokes-twitter_55b7ae8de4b0224d8833fdf4",
+	source: "Huffington Post, 28 July 2015",
+};
+
+var allegedly = [coach, lord, conan];
+
+var birthday = {
+	headline: "The 'Happy Birthday' song could soon be free",
+	url: "http://www.latimes.com/entertainment/music/posts/la-et-ms-happy-birthday-copyright-20150728-story.html",
+	source: "Los Angeles Times, 29 July 2015",
+};
+
+var solar = {
+	headline: "Earth could get just 12 hours' warning of damaging solar storm",
+	url: "http://www.theguardian.com/science/2015/jul/28/uk-can-expect-just-12-hours-warning-damaging-solar-storm-space-weather",
+	source: "The Guardian, 28 July 2015",
+};
+
+var fed = {
+	headline: "Faster pace of Fed hikes could spook market",
+	url: "http://www.usatoday.com/story/money/markets/2015/07/29/fed-meeting-rate-hikes-impact/30817881/",
+	source: "USA Today, 29 July 2015",
+};
+
+could = [birthday, solar, fed];
