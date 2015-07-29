4ebc4a44e774c50e8d79957b16f3f6331f29583a
marbiru <code@uribram.com>
2015-07-29 13:16:18 +0100

    Textillate Working
    Can now animate text

---
+++ js/headlines.js
@@ -0,0 +1,17 @@
+var insulin = {
+	headline: "Insulin resistance might increase Alzheimer’s risk", 
+	url: "http://www.reuters.com/article/2015/07/28/us-health-alzheimers-diabetes-idUSKCN0Q22I720150728", 
+	source:"Reuters, 28 July 2015",
+};
+
+var streetlights = {
+	headline: "Streetlights Might Not Make You Safer",
+	url: "http://www.psmag.com/politics-and-law/streetlights-might-not-make-you-safer",
+	source: "Pacific Standard, 28 July 2015",
+};
+
+var corbyn = {
+	headline: "Jeremy Corbyn might just be able to unite Britain's progressive parties",
+	url: "http://www.theguardian.com/politics/2015/jul/26/jeremy-corbyn-might-just-be-able-to-unite-britains-progressive-parties",
+	source: "The Guardian, 26 July 2015",
+};
