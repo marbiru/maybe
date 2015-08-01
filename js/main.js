ceea1b4b20a8de3bcd2d00571b9874b9e2d454cf
marbiru <code@uribram.com>
2015-08-01 14:58:07 +0100

    feat: text on mouseclick
    text now only starts animating on a card when the card is shown i.e.
    after previous card has been clicked

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('.tlt_headlines').textillate({ 
+	$( ".first .tlt_headlines" ).textillate({ 
 		in: { 
 			effect: 'rollIn',
 			delayScale: 1, 
@@ -7,9 +7,8 @@ $(function () {
 	});
 });
 
-// need version that only loads on mouseclick
 $(function () {
-	$('.tlt_source').textillate({ 
+	$( ".first .tlt_source" ).textillate({ 
 		initialDelay: 1500,
 		in: {
 		 effect: 'rollIn',
@@ -19,29 +18,72 @@ $(function () {
 	});
 });
 
-// eventually this should be extracted as a function
+// on mouseclick
+
+$(function () {
+	$( ".second .tlt_headlines, .third .tlt_headlines" ).textillate({ 
+		autoStart: false,
+		in: { 
+			effect: 'rollIn',
+			delayScale: 1, 
+		} 
+	});
+});
+
+$(function () {
+	$( ".second .tlt_source, .third .tlt_source" ).textillate({
+		autoStart: false,
+		//this seems to require a longer delay on mouseclick ones than on equivalent pageload ones -- wonder why?
+		initialDelay: 2500,
+		in: {
+		 effect: 'rollIn',
+		 delayScale: 0.3,
+		 shuffle: true,
+		}
+	});
+});
+
+
+// eventually the below should be extracted as a function
+
+/* function transition(hedge) {
+	
+	$( ".might.first" ).click(function() {
+  		$( ".might.second" ).show();
+	});
+
+	$( ".might.second" ).click(function() {
+  		$( ".might.third" ).show();
+	});
+}; */
 
 $( ".might.first" ).click(function() {
   $( ".might.second" ).show();
+  $( ".might.second .tlt_headlines, .might.second .tlt_source" ).textillate('start');
 });
 
 $( ".might.second" ).click(function() {
   $( ".might.third" ).show();
+  $( ".might.third .tlt_headlines, .might.third .tlt_source" ).textillate('start');
 });
 
 
 $( ".allegedly.first" ).click(function() {
   $( ".allegedly.second" ).show();
+  $( ".allegedly.second .tlt_headlines, .allegedly.second .tlt_source" ).textillate('start');
 });
 
 $( ".allegedly.second" ).click(function() {
   $( ".allegedly.third" ).show();
+  $( ".allegedly.third .tlt_headlines, .allegedly.third .tlt_source" ).textillate('start');
 });
 
 $( ".could.first" ).click(function() {
   $( ".could.second" ).show();
+  $( ".could.second .tlt_headlines, .could.second .tlt_source" ).textillate('start');
 });
 
 $( ".could.second" ).click(function() {
   $( ".could.third" ).show();
+  $( ".could.third .tlt_headlines, .could.third .tlt_source" ).textillate('start');
 });
